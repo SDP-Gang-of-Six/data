160fcad555fc5fa3cf2df1ac4950f4d56bcb24ee
pc <2528578653@qq.com>
2024-03-13 16:45:06 +0800

    用户登录和分页查询功能实现1.0

--- src/main/java/cn/wxl475/minio/MinioConfig.java
+++ src/main/java/cn/wxl475/minio/MinioConfig.java
@@ -1,6 +1,7 @@
 package cn.wxl475.minio;
 
-import io.minio.MinioClient;
+
+import io.minio.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
