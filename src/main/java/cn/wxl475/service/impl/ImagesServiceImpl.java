bbcb81ad4fbd8c90d945c6ec4eb673bfa478c80a
wxl <1715142548@qq.com>
2024-03-18 22:30:12 +0800

    nginx文件上传

--- src/main/java/cn/wxl475/service/impl/ImagesServiceImpl.java
+++ src/main/java/cn/wxl475/service/impl/ImagesServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.wxl475.service.impl;
 
 import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.URLUtil;
 import cn.wxl475.mapper.ImagesMapper;
 import cn.wxl475.pojo.Image;
 import cn.wxl475.service.ImagesService;
@@ -11,7 +12,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.Future;
 
@@ -25,9 +30,12 @@ public class ImagesServiceImpl implements ImagesService {
     @Value("${fileServer.urlPrefix}")
     private String urlPrefix;
 
+    private final String imagesPathInVM = "/data/pet-hospital/images/";
+
 
     /**
      * 上传图片
+     *
      * @param images
      * @param userId
      * @return
@@ -38,24 +46,38 @@ public class ImagesServiceImpl implements ImagesService {
         // 上传文件
         CompletionService<Image> completionService = ThreadUtil.newCompletionService();
         ArrayList<Future<Image>> futures = new ArrayList<>();
-        for(MultipartFile image : images){
+        for (MultipartFile image : images) {
             futures.add(completionService.submit(() -> {
-
-                return new Image();
+                Image image1 = new Image(
+                        null,
+                        userId,
+                        null,
+                        image.getOriginalFilename(),
+                        image.getContentType(),
+                        image.getSize(),
+                        null,
+                        null,
+                        false
+                );
+                String newFileName = new SimpleDateFormat("yyyy-MM-dd-hh:mm:ss").format(new Date()) + "_" + image.getOriginalFilename();
+                Files.write(Path.of(imagesPathInVM + newFileName), image.getBytes());
+                image1.setImageUrl(URLUtil.normalize(
+                            urlPrefix +
+                                "/images/" +
+                                newFileName
+                ));
+                return image1;
             }));
         }
         ArrayList<Image> imageList = new ArrayList<>();
-        for(Future<Image> future : futures){
+        for (Future<Image> future : futures) {
             Image image = new Image();
             try {
                 image = future.get();
             } catch (Exception e) {
-                log.error("线程结果获取失败", e);
+                log.error("写文件线程结果获取失败，该线程索引："+futures.indexOf(future), e);
             }
-            String url = image.getImageUrl();
-            if(!("上传文件为空".equals(url) || "上传文件失败".equals(url))){
-                imagesMapper.insert(image);
-            }
+            imagesMapper.insert(image);
             imageList.add(image);
         }
         return imageList;
