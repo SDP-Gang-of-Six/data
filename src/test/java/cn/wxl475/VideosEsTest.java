813c61936c7cef57c4ea0fa6839e4cdfc4bf6129
wxl <1715142548@qq.com>
2024-03-25 21:07:33 +0800

    VideosEsTest.java

---
+++ src/test/java/cn/wxl475/VideosEsTest.java
@@ -0,0 +1,54 @@
+package cn.wxl475;
+
+import cn.wxl475.pojo.data.Video;
+import cn.wxl475.repo.VideoEsRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@SpringBootTest(args = "--spring.profiles.active=dev")
+@RunWith(SpringRunner.class)
+public class VideosEsTest {
+    @Autowired
+    private ElasticsearchRestTemplate elasticsearchRestTemplate;
+
+    @Autowired
+    private VideoEsRepo videoEsRepo;
+
+    @Test
+    public void creatVideosIndex(){
+        Class<Video> aClass = Video.class;
+        boolean created = elasticsearchRestTemplate.indexOps(aClass).createWithMapping();
+        if(created){
+            log.info("创建索引：{}",aClass.getName()+",成功");
+        }else {
+            log.info("创建索引：{}",aClass.getName()+",失败");
+        }
+    }
+
+    @Test
+    public void deleteVideosIndex(){
+        Class<Video> aClass = Video.class;
+        boolean deleted = elasticsearchRestTemplate.indexOps(aClass).delete();
+        if(deleted){
+            log.info("删除索引：{}",aClass.getName()+",成功");
+        }else {
+            log.info("删除索引：{}",aClass.getName()+",失败");
+        }
+    }
+
+    @Test
+    public void findAllVideos(){
+        videoEsRepo.findAll().forEach(video -> { log.info(String.valueOf(video));});
+    }
+
+    @Test
+    public void deleteAllVideos(){
+        videoEsRepo.deleteAll();
+    }
+}
