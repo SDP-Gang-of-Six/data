4ceceb8647d9f5edd889058d2c41686bf7bb0a7a
wxl <1715142548@qq.com>
2024-03-26 14:53:13 +0800

    图片缩放测试

--- src/test/java/cn/wxl475/FileTest.java
+++ src/test/java/cn/wxl475/FileTest.java
@@ -5,6 +5,7 @@ import cn.hutool.core.io.FileUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
@@ -33,5 +34,12 @@ public class FileTest {
     @Test
     public void imageConvert(){
         ImgUtil.convert(FileUtil.file("D:/学期资料/图集/新建文件夹/1 (2).jpg"), FileUtil.file("D:/学期资料/图集/新建文件夹/1 (2).png"));
+    }
+
+    @Test
+    public void scale(){
+        File file = FileUtil.file("D:/学期资料/图集/新建文件夹/1 (2).jpg");
+        File file1 = FileUtil.file("D:/学期资料/图集/新建文件夹/1.jpg");
+        ImgUtil.scale(file,file1, 0.1F);
     }
 }
